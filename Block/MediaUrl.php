aa771f4702ffc4af43ccd246da944a284e6d84a2
David Windell <david@outeredgeuk.com>
2023-08-03 14:46:01 +0100

    Update MediaUrl.php

--- Block/MediaUrl.php
+++ Block/MediaUrl.php
@@ -2,20 +2,20 @@
 
 namespace OuterEdge\SirvOptimisations\Block;
 
+use Magento\Framework\UrlInterface;
 use Magento\Framework\View\Element\Template;
 
 class MediaUrl extends Template
 {
     public function getMediaUrl()
     {
-        $mediaUrl = $this ->_storeManager->getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_MEDIA);
+        $mediaUrl = $this ->_storeManager->getStore()->getBaseUrl(UrlInterface::URL_TYPE_MEDIA);
+        
         if ($mediaUrl) {
             $parsedUrl = parse_url($mediaUrl);
-            $url = $parsedUrl['scheme'] . '://' . $parsedUrl['host'];
-
-            return $url;
+            $mediaUrl  = $parsedUrl['scheme'] . '://' . $parsedUrl['host'];
         }
-        return null;
+        
+        return $mediaUrl;
     }
 }
-?>
