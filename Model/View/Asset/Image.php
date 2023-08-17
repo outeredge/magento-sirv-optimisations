9ec06e051e1bd5d1bbaa6ae2e41a18c07be7d802
Bruno Dias <ibroopi@gmail.com>
2023-08-17 12:17:58 +0100

    Add settings to sirv (#2)
    * Add settings to sirv
    
    * Update Image.php
    
    ---------
    
    Co-authored-by: David Windell <david@outeredgeuk.com>

--- Model/View/Asset/Image.php
+++ Model/View/Asset/Image.php
@@ -2,16 +2,49 @@
 
 namespace OuterEdge\SirvOptimisations\Model\View\Asset;
 
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Catalog\Model\Product\Media\ConfigInterface;
+use Sirv\Magento2\Model\View\Asset\Image\Context;
+use Magento\Framework\Encryption\EncryptorInterface;
 use Sirv\Magento2\Model\View\Asset\Image as SirvImage;
 
 class Image extends SirvImage
 {
+    protected $scopeConfig;
+
+    public function __construct(
+        ConfigInterface $mediaConfig,
+        Context $context,
+        EncryptorInterface $encryptor,
+        ScopeConfigInterface $scopeConfig,
+        $filePath,
+        array $miscParams = []
+    ) {
+        parent::__construct(
+            $mediaConfig,
+            $context,
+            $encryptor,
+            $scopeConfig,
+            $filePath,
+            $miscParams
+        );
+    }
+
     /*
      * Apply crop trim to all images
      */
     protected function getUrlQuery($absPath)
     {
+        $enableCropTrim = $this->scopeConfig->getValue(
+            'oe_sirv/settings/enable_crop_trim',
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
+        );
+
         $query = parent::getUrlQuery($absPath);
+        
+        if (!$enableCropTrim) {
+            return $query;
+        }
 
         if (empty($query) || (
             !stristr($absPath, '/catalog/product/') &&
